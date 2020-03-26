ae52ae466b85cb404fe25935bf5baaef572666ac
thealexguy1 <60763681+thealexguy1@users.noreply.github.com>
2020-03-26 16:20:47 +1300

    Update alexwebsite.py

--- alexwebsite.py
+++ alexwebsite.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
+import jinja2
+import trademeScraper
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    productlist = trademeScraper.Scrape()
+    average = trademeScraper.GetAverage(productlist)
+    return render_template("home.html", productlist=productlist, average=average)
 
 if __name__ == "__main__":
     app.run(debug=True)
